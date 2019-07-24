aeb9bf7ea226933c6f339c037c0c6467c91cf3c1
rafal.slosarz <rafalslosarz@gmail.com>
2019-07-24 23:44:50 +0200

    Pass CharSequence credentials to BaseAuthentication

--- app/src/main/java/com/slosarz/stocksearch/network/BaseAuthenticationInterceptor.kt
+++ app/src/main/java/com/slosarz/stocksearch/network/BaseAuthenticationInterceptor.kt
@@ -5,9 +5,9 @@ import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class BaseAuthenticationInterceptor(user: String, password: String) : Interceptor {
+class BaseAuthenticationInterceptor(user: CharSequence, password: CharSequence) : Interceptor {
 
-    private val credentials: String = Credentials.basic(user, password)
+    private val credentials: String = Credentials.basic(user.toString(), password.toString())
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
